c6893345cbddd298e5f6cd7a50f19e204e2d4d3d
Abdulaziz Jamal <fy12aaj@leeds.ac.uk>
2015-12-01 23:49:03 +0000

    Added timer object when calculate time in the log.

--- MyFit/MyFit/ViewController.swift
+++ MyFit/MyFit/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer() // Object that control the time according the var.
+    
+    var count = 0 // Var count start from 0 for the timer object.
+    
+    func result() { // Results function
+        
+        count++ // count 1.
+        println(count) // appear in the log.
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true) // timer object which tick every 1 secound with one view controler, method results run every secound.
+        
     }
 
     override func didReceiveMemoryWarning() {
