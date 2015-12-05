e30bb4042e03aa621e90e67753a7aeb9174c04a4
Abdulaziz Jamal <fy12aaj@leeds.ac.uk>
2015-12-05 03:21:48 +0000

    Added a ViewCalculate swift file, and change a bit of view controller.

---
+++ MyFit/MyFit/ViewCalculate.swift
@@ -0,0 +1,10 @@
+//
+//  ViewCalculate.swift
+//  MyFit
+//
+//  Created by Abdulaziz Jamal on 12/5/15.
+//  Copyright (c) 2015 Abdulaziz Jamal. All rights reserved.
+//
+
+import Foundation
+import UIKit
