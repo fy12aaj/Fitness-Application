3c8d27dbdb352fe0b4fbf4cf19a7403a994ea971
Abdulaziz Jamal <fy12aaj@leeds.ac.uk>
2015-12-08 18:54:57 +0000

    New project with updated storyboard has been uploaded

---
+++ MyFit v.1/MyFit/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MyFit
+//
+//  Created by Abdulaziz Jamal on 12/6/15.
+//  Copyright (c) 2015 Abdulaziz Jamal. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theMap: MKMapView!
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
