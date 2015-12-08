a5b014786499435e785a0f42b6af30735e3c3ef5
Abdulaziz Jamal <fy12aaj@leeds.ac.uk>
2015-12-08 23:12:11 +0000

    finished the BMI covertable, just a small bits need to be changed.

---
+++ MyFit v.1/MyFit/BMICalculator.swift
@@ -0,0 +1,60 @@
+//
+//  BMICalculator.swift
+//  MyFit
+//
+//  Created by Abdulaziz Jamal on 12/8/15.
+//  Copyright (c) 2015 Abdulaziz Jamal. All rights reserved.
+//
+
+import UIKit
+
+class BMICalculator: UIViewController {
+    
+
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var genderControl: UISegmentedControl!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    
+    @IBAction func weightChanged(sender: AnyObject) {
+        weightLabel.text = String(stringInterpolationSegment: weightSlider.value) + "lbs"
+    }
+    
+    
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            performSegueWithIdentifier("showres", sender: self)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        genderControl.selectedSegmentIndex = 0
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        let detailController = segue.destinationViewController as! BMIResultViewController
+        detailController.height = Int(heightTextField.text!)
+        detailController.weight = Int(weightSlider.value)
+        detailController.gender = Int(genderControl.selectedSegmentIndex)
+        
+    }
+    
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
