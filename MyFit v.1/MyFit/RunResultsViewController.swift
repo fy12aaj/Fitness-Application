06691f32a9baa5dc5c2679333cda858eb8401672
Abdulaziz Jamal <fy12aaj@leeds.ac.uk>
2015-12-14 08:41:42 +0000

    commented RunResultsViewController file

--- MyFit v.1/MyFit/RunResultsViewController.swift
+++ MyFit v.1/MyFit/RunResultsViewController.swift
@@ -21,8 +21,8 @@ class RunResultsViewController: UIViewController {
     @IBOutlet weak var paceLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
-    @IBOutlet weak var facebookButton: UIButton!
-    @IBOutlet weak var twitterButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton! //Facebook Button
+    @IBOutlet weak var twitterButton: UIButton! // Twitter Button.
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,25 +35,25 @@ class RunResultsViewController: UIViewController {
     }
     //MARK: Displaying Run
     
-    func configureView(){
+    func configureView(){ // Display Distance in meters.
         let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: run!.distance.doubleValue)
         distanceLabel.text = "Distance: " + distanceQuantity.description
         
-        let dateFormatter = NSDateFormatter()
+        let dateFormatter = NSDateFormatter() // Calculate the date.
         dateFormatter.dateStyle = .MediumStyle
         dateLabel.text = dateFormatter.stringFromDate(run!.timestamp)
         
-        let secondsQuantity = HKQuantity(unit: HKUnit.secondUnit(), doubleValue: run!.duration.doubleValue)
+        let secondsQuantity = HKQuantity(unit: HKUnit.secondUnit(), doubleValue: run!.duration.doubleValue) // Calculate the time.
         timeLabel.text = "Time: " + secondsQuantity.description
         
-        let paceUnit = HKUnit.meterUnit().unitDividedByUnit(HKUnit.secondUnit())
+        let paceUnit = HKUnit.meterUnit().unitDividedByUnit(HKUnit.secondUnit()) // Calculate the pace.
         let paceQuantity = HKQuantity(unit: paceUnit, doubleValue: run!.distance.doubleValue / run!.duration.doubleValue)
         paceLabel.text = "Pace: " + paceQuantity.description
         
         loadMap()
     }
     
-    func mapRegion() -> MKCoordinateRegion {
+    func mapRegion() -> MKCoordinateRegion { // Calculate map region to display the latitude and longitude.
         let initialLoc = run!.locations.firstObject as! Location
         
         var minLat = initialLoc.latitude.doubleValue
@@ -70,29 +70,29 @@ class RunResultsViewController: UIViewController {
             maxLng = max(maxLng, location.longitude.doubleValue)
         }
         
-        return MKCoordinateRegion(
+        return MKCoordinateRegion( // Calculte the coordinates of the location.
             center: CLLocationCoordinate2D(latitude: (minLat + maxLat)/2,
                 longitude: (minLng + maxLng)/2),
             span: MKCoordinateSpan(latitudeDelta: (maxLat - minLat)*1.1,
                 longitudeDelta: (maxLng - minLng)*1.1))
     }
     
-    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! { // Draw's a polyline in the map. ( for some reasons it does not appear in the map viewed).
         if !overlay.isKindOfClass(MKPolyline) {
             return nil
         }
         
         let polyline = overlay as! MKPolyline
         let renderer = MKPolylineRenderer(polyline: polyline)
-        renderer.strokeColor = UIColor.blueColor()
+        renderer.strokeColor = UIColor.blueColor() // Color of polyline.
         renderer.lineWidth = 3
         return renderer
     }
     
-    func polyline() -> MKPolyline {
+    func polyline() -> MKPolyline { // Polyline takes the CLocation for 2D coordinate.
         var coords = [CLLocationCoordinate2D]()
         
-        let locations = run!.locations.array as! [Location]
+        let locations = run!.locations.array as! [Location] // locations array.
         for location in locations {
             coords.append(CLLocationCoordinate2D(latitude: location.latitude.doubleValue,
                 longitude: location.longitude.doubleValue))
@@ -126,7 +126,7 @@ class RunResultsViewController: UIViewController {
     
     @IBAction func twitterAction(sender: UIButton) {
         
-        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {        // Check if the user has their Twitter Application Installed and login.
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {        // Check if the user has the Twitter Application Installed and login.
             
             var tweetShare:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
             
